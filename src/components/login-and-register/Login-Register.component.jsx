cfd65d3fa88043f99d1bb3bf506cb5c1a251943d
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-08-30 22:23:55 -0500

    sign in w/email and password

--- src/components/login-and-register/Login-Register.component.jsx
+++ src/components/login-and-register/Login-Register.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Login from '../Login/Login.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './Login-Register.styles.scss';
 
 const LoginAndRegister = () => (
   <div className='sign-in-and-sign-up'>
     <Login />
+    <SignUp />
   </div>
 );
 
